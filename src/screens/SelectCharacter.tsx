89a1a580ed10f5fee42a7b5c6ab5e057460b1849
MichaelXerxes <hiszpan4444@googlemail.com>
2023-07-06 16:05:10 +0100

    clean up and get ready ...

--- src/screens/SelectCharacter.tsx
+++ src/screens/SelectCharacter.tsx
@@ -20,70 +20,12 @@ const SelectCharacter: React.FC<Props> = ({ navigation }) => {
   const anim3 = useRef(new Animated.Value(-500)).current;
   const isFocused = useIsFocused();
 
-  useEffect(() => {
-    const animation =
-      //Animated.loop(
-      Animated.parallel([
-        Animated.spring(anim1, {
-          toValue: 0,
-          useNativeDriver: false,
-          speed: 0.05,
-        }),
-        Animated.spring(anim2, {
-          toValue: 0,
-          useNativeDriver: false,
-          speed: 1,
-        }),
-        Animated.spring(anim3, {
-          toValue: 0,
-          useNativeDriver: false,
-          speed: 1,
-        }),
-      ]);
-
-    // );
-
-    const middleAnimation = Animated.loop(
-      Animated.sequence([
-        Animated.delay(2000),
-        Animated.timing(anim2, {
-          toValue: -120,
-          duration: 3000,
-          useNativeDriver: false,
-        }),
-        Animated.timing(anim2, {
-          toValue: 0,
-          duration: 3000,
-          useNativeDriver: false,
-        }),
-      ])
-    );
-
-    if (isFocused) {
-      animation.start(() => middleAnimation.start());
-    }
-    //  else {
-    //   animation.stop();
-    // }
-
-    return () => {
-      animation.stop();
-      middleAnimation.stop();
-      anim1.setValue(-500);
-      anim2.setValue(500);
-      anim3.setValue(-500);
-      animMiddle.setValue(0);
-    };
-  }, [anim1, anim2, anim3, isFocused]);
   return (
     <ImageBackground
       source={require("../assets/ai-images/endgame.jpg")}
       style={styles.container}
     >
-      {/* <Ball2 /> */}
-      {/* <View style={styles.container}> */}
       <ViewGameEngine />
-      {/* </View> */}
     </ImageBackground>
   );
 };
